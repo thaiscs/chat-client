51f6f912bcada715df6f43857043371e4eda58fb
thaiscs <thais.advj@gmail.com>
2019-12-13 14:25:14 +0100

    client-side setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  stream = new EventSource("http://localhost:4000/stream");
+
+  componentDidMount() {
+    this.stream.onmessage = event => {
+      console.log("event test: ", event.data);
+
+      const parse = JSON.parse(event.data); // back to js data types
+      console.log("parsed: ", parse);
+    };
+  }
+
+  render() {
+    return <div>cliente</div>;
+  }
 }
 
 export default App;
